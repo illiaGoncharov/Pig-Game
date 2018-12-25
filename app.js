bcc54cf61487887ea828eb121297e2ca544bb35a
illia GG <ilia.m.goncharov@gmail.com>
2018-12-25 04:47:58 +0300

    v2.0

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- The first player to reach 200 or Number Specifided By Player points on GLOBAL score wins the game
 
 */
+
+alert("The game has 2 players, playing in rounds");
+alert("In each turn, a player rolls a dice as many times as he whishes.");
+alert("Each result get added to his ROUND score.");
+alert("BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn.");
+alert("AND, if the player rolls two 6, ALL his scores get lost. After that, it's the next player's turn.");
+alert("The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn.");
+alert("The first player to reach 200 or Specified Winner Score (bottom of the page) points on GLOBAL score wins the game.");
+alert("You can see whole Game Log in Browser Console (cmd + opt + i then Console)");
+alert("Have Fun!");
 
 var scores, 
 	roundScore,
 	activePlayer,
-	gamePlaying;
+	gamePlaying,
+	firstDice,
+	secondDice;
 	
 init();
 
@@ -21,21 +33,44 @@ function nextPlayer() {
 	document.getElementById('current-' + activePlayer).textContent = '0';
 	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 	roundScore = 0;
-	document.querySelector('.dice').style.display = 'none';
+	document.querySelector('.dice-1').style.display = 'none';
+	document.querySelector('.dice-2').style.display = 'none';
 	document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+
+	console.log("Player-" + activePlayer + " now playes!");
 };
+
+function doubleSix() {
+	roundScore = 0;
+	document.querySelector('#current-' + activePlayer).textContent = roundScore; 
+	// Add current to global
+	score[activePlayer] = roundScore;
+	// Update UI
+	document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+	console.log('Player-' + activePlayer + ": " + "6x2: Bad Luck");
+	nextPlayer();
+}
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 		if (gamePlaying) {
-			var dice = Math.floor(Math.random() * 6) + 1; 
-			var diceDOM = document.querySelector('.dice');
-			document.querySelector('.dice').style.display = 'block';
-			diceDOM.src = 'dice-' + dice + '.png';
-			if(dice !== 1){
+			firstDice = Math.floor(Math.random() * 6) + 1;
+			secondDice = Math.floor(Math.random() * 6) + 1;
+			console.log('Player-' + activePlayer + ": " + firstDice + " and " + secondDice);
+			var diceDOMone = document.querySelector('.dice-1');
+			var diceDOMtwo = document.querySelector('.dice-2');
+			document.querySelector('.dice-1').style.display = 'block';
+			document.querySelector('.dice-2').style.display = 'block';
+			diceDOMone.src = 'dice-' + firstDice + '.png';
+			diceDOMtwo.src = 'dice-' + secondDice + '.png';
+			if(firstDice === 6 && secondDice === 6){
+				doubleSix();
+			} else if (firstDice !== 1 && secondDice !== 1) {
 				// Add score
+				var dice = firstDice + secondDice;
 				roundScore += dice;
 				document.querySelector('#current-' + activePlayer).textContent = roundScore; 
 			} else {
+				console.log("Other player's turn");				
 				// Other player
 				nextPlayer();
 			}
@@ -50,11 +85,22 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 
 			// Update UI
 			document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+			console.log('Player-' + activePlayer + " now holds: " + score[activePlayer]);
+
+			// Check Winnig Score
+			var input = document.getElementById('input').value;
+			var winningScore;
+			if (input) {
+				winningScore = input;
+			} else {
+				winningScore = 200;
+			}
 
 			// Check if player won
-			if (score[activePlayer] >= 20) {
+			if (score[activePlayer] >= winningScore) {
 				document.getElementById('name-' + activePlayer).textContent = "Winner!";
-				document.querySelector('.dice').style.display = 'none';
+				document.querySelector('.dice-1').style.display = 'none';
+				document.querySelector('.dice-2').style.display = 'none';
 				document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner');
 				document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
 				gamePlaying = false;
@@ -71,15 +117,16 @@ function init() {
 	activePlayer = 0;
 	gamePlaying = true;
 
-	document.querySelector('.dice').style.display = 'none';
+	document.querySelector('.dice-1').style.display = 'none';
+	document.querySelector('.dice-2').style.display = 'none';
 
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
 	document.getElementById('current-0').textContent = '0';
 	document.getElementById('current-1').textContent = '0';
 
-	document.getElementById('name-0').textContent = "Player 1";
-	document.getElementById('name-1').textContent = "Player 2";
+	document.getElementById('name-0').textContent = "Player 0";
+	document.getElementById('name-1').textContent = "Player 1";
 
 	document.querySelector('.player-0-panel').classList.remove('winner');
 	document.querySelector('.player-1-panel').classList.remove('winner');
@@ -88,11 +135,9 @@ function init() {
 	document.querySelector('.player-1-panel').classList.remove('active');
 
 	document.querySelector('.player-0-panel').classList.add('active');
+
+	console.log("Game Init: Active Player-" + activePlayer)
 };
 
 document.querySelector('.btn-new').addEventListener('click', init);
-
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// var x = document.querySelector('#score-0').textContent;
  
---
+++ readme.txt
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1251\cocoartf1671\cocoasubrtf200
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 GAME RULES:  - The game has 2 players, playing in rounds - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn - The first player to reach 200 or Number Specifided By Player points on GLOBAL score wins the game  }
